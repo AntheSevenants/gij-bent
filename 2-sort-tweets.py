1123364209329e0bcb4df1211f480c41cf655ba2
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-04-20 20:35:18 +0200

    Finish sorting script

--- 2-sort-tweets.py
+++ 2-sort-tweets.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import re
 import pandas as pd
 
 from glob import glob
@@ -9,7 +10,7 @@ from tqdm.auto import tqdm
 parser = argparse.ArgumentParser(
     description='sort-tweets - convert tweets to TSV and keep only relevant ones')
 parser.add_argument('corpus_directory', type=str, help='path to the directory with all retrieved tweets')
-parser.add_argument('working-tsv', type=str, help='The TSV which contains all current tweets')
+parser.add_argument('working_tsv', type=str, help='The TSV which contains all current tweets')
 
 args = parser.parse_args()
 
@@ -35,4 +36,50 @@ for jsonl_file in tqdm(jsonl_files, desc="jsonl files"):
                 if "Bel" in tweet["place"][1]:
                     good_tweets.append((construction_type, tweet))
 
-print(f"Found {len(good_tweets)} good tweets")
+rows = []
+for good_tweet in tqdm(good_tweets, desc="Converting good tweets"):
+    construction_type = good_tweet[0]
+    tweet = good_tweet[1]
+
+    tweet_dict = {
+        "construction_type": construction_type,
+        "year": re.findall("^(\d{4})", tweet["date"])[0],
+        "date": tweet["date"],
+        "content": tweet["content"],
+        "id": tweet["id"],
+        "user_id": tweet["user_id"],
+        "username": tweet["username"],
+        "user_display_name": tweet["user_display_name"],
+        "url": tweet["url"],
+        "user_verified": tweet["user_verified"],
+        "year_user_created": re.findall("^(\d{4})", tweet["user_created"])[0],
+        "user_followers_count": tweet["user_followers_count"],
+        "user_friends_count": tweet["user_friends_count"],
+        "user_tweet_count": tweet["user_tweet_count"],
+        "user_favourites_count": tweet["user_favourites_count"],
+        "user_listed_count": tweet["user_listed_count"],
+        "user_media_count": tweet["user_media_count"],
+        "user_location": tweet["user_location"],
+        "user_profile_image_url": tweet["user_profile_image_url"],
+        "reply_count": tweet["reply_count"],
+        "retweet_count": tweet["retweet_count"],
+        "like_count": tweet["like_count"],
+        "quote_count": tweet["quote_count"],
+        "source_label": tweet["source_label"],
+        #"links": tweet.links,
+        "mentioned_users_count": tweet["mentioned_users_count"],
+        "lat": tweet["coordinates"][0],
+        "long": tweet["coordinates"][0],
+        "place_id": tweet["place"][0],
+        "place_full_name": tweet["place"][1],
+        "place_reported_name": tweet["place"][2],
+        "hashtag_count": len(tweet["hashtags"]) if tweet["hashtags"] is not None else 0,
+    }
+
+    rows.append(tweet_dict)
+
+df = pd.DataFrame(rows)
+
+df.to_csv(args.working_tsv, sep="\t", index=False)
+
+print(f"Written output to {args.working_tsv}")
