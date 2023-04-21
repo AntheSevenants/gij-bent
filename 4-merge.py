61697fd84f152edc244c422dbc9239ba975ac069
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-04-21 09:45:46 +0200

    Implement data merging code

---
+++ 4-merge.py
@@ -0,0 +1,30 @@
+import argparse
+import pandas as pd
+
+import argparse
+import pandas as pd
+
+parser = argparse.ArgumentParser(
+    description='geoguess - provide latlong and dialect area information for each tweet')
+parser.add_argument('working_tsv', type=str,
+                    help='The TSV which contains all current tweets')
+parser.add_argument('geo_tsv', type=str,
+                    help='The TSV which contains all geo information')
+parser.add_argument('--output_tsv', type=str, default="tweets_geo.tsv",
+                    help='The TSV to output the merged files to')
+
+args = parser.parse_args()
+
+# Read both dataframes
+df_tweets = pd.read_csv(args.working_tsv, sep="\t")
+df_geo = pd.read_csv(args.geo_tsv, sep="\t")
+
+# "lat" and "long" also exist in the Twitter data
+# so I provide new names for them before merging
+df_geo = df_geo.rename(columns={"lat": "norm_lat", "long": "norm_long"})
+
+# Inner join
+df_tweets_geo = pd.merge(df_tweets, df_geo, on="id")
+
+# Output
+df_tweets_geo.to_csv(args.output_tsv, index=False, sep="\t")
