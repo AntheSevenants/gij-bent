fe34fab52e92f190bbbb8a304ccc5b156ec28a78
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-05-04 12:02:51 +0200

    Add gender data source to merging script

--- 5-merge.py
+++ 5-merge.py
@@ -7,14 +7,17 @@ parser.add_argument('working_tsv', type=str,
                     help='The TSV which contains all current tweets')
 parser.add_argument('geo_tsv', type=str,
                     help='The TSV which contains all geo information')
+parser.add_argument('gender_tsv', type=str,
+                    help='The TSV which contains all gender information')
 parser.add_argument('--output_tsv', type=str, default="tweets_geo.tsv",
                     help='The TSV to output the merged files to')
 
 args = parser.parse_args()
 
-# Read both dataframes
+# Read all dataframes
 df_tweets = pd.read_csv(args.working_tsv, sep="\t")
 df_geo = pd.read_csv(args.geo_tsv, sep="\t")
+df_gender = pd.read_csv(args.gender_tsv, sep="\t")
 
 # "lat" and "long" also exist in the Twitter data
 # so I provide new names for them before merging
@@ -30,5 +33,7 @@ df_tweets_geo = pd.merge(df_tweets, df_geo, on=["id", "user_id"], how="left")
 # Remove tweets where dialect is False
 df_tweets_geo = df_tweets_geo.loc[df_tweets_geo["dialect"] != "False"]
 
+df_tweets_geo_gender = pd.merge(df_tweets_geo, df_gender, on=["user_display_name"], how="left")
+
 # Output
-df_tweets_geo.to_csv(args.output_tsv, index=False, sep="\t")
+df_tweets_geo_gender.to_csv(args.output_tsv, index=False, sep="\t")
