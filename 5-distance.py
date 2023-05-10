e6fd24be03445843931eaf2ac2094c2af918df0c
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-05-10 17:52:12 +0200

    Compute distance between subject and predicate

--- 5-distance.py
+++ 5-distance.py
@@ -39,7 +39,7 @@ for index, row in tqdm(df.iterrows(), total=len(df), desc="Tweets processed"):
         if len(token) >= 2:
             parts = token.split(".")
         else:
-            parts = [ token ]
+            parts = [token]
 
         _ = _ + parts
     tokens = _
@@ -56,16 +56,45 @@ for index, row in tqdm(df.iterrows(), total=len(df), desc="Tweets processed"):
         if re.match(f".*({'|'.join(needles)}).*", token):
             predicate_index = idx
 
-        if re.match(f"(ge|gij|gy)", token):
+        if re.match(f".*(ge|gij|gy|\\wg\\w).*", token):
             subject_indices.append(idx)
 
     # If no predicate found at the end, value will remain None
     if predicate_index is None:
-        pass
+        continue
 
     # If no subject found at the end, value will remain None
     if len(subject_indices) == 0:
+        continue
         not_found.append(tokens)
+
+    context = None
+    distance = None
+    # We test for the possible contexts where gij zijt/gij bent can appear
+    if predicate_index - 1 in subject_indices:
+        context = "main"
+        distance = 1
+    elif predicate_index + 1 in subject_indices:
+        context = "inversion"
+        distance = 1
+    else:
+        context = "other"
+        # We find the subject closest to the predicate
+        distances = list(map(lambda x: predicate_index - x, subject_indices))
+        distances = list(filter(lambda x: x > 0, distances))
+
+        try:
+            distance = min(distances)
+        except:
+            not_found.append([subject_indices, predicate_index, row["content"]])
+
+    new_rows.append({"id": row["id"],
+                     "user_id": row["user_id"],
+                     "context": context,
+                     "distance": distance})
+
+distance_df = pd.DataFrame(new_rows)
+distance_df.to_csv(args.output_tsv, index=False, sep="\t")
 
 with open("error.json", "wt") as writer:
     writer.write(json.dumps(not_found))
